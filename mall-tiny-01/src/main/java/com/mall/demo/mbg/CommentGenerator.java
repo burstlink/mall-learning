6e2c92c89eb4de5472a46757aea6334ad7cd6dad
Li <liyy.fnst@fujitsu.com>
2021-04-25 15:39:37 +0800

    update0425

--- mall-tiny-01/src/main/java/com/mall/demo/mbg/CommentGenerator.java
+++ mall-tiny-01/src/main/java/com/mall/demo/mbg/CommentGenerator.java
@@ -3,7 +3,6 @@ package com.mall.demo.mbg;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
-import org.mybatis.generator.api.dom.kotlin.KotlinFile;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 import org.mybatis.generator.internal.util.StringUtility;
 
